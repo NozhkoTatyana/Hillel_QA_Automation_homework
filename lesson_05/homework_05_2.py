a67a6756d226159bcc562c18bbd24b201daca4c6
NozhkoTatyana <tatyana.nozhko@ukr.net>
2026-03-09 10:43:52 +0200

    Додано рішення для homework5_2: додавання запису,перестановка елементів, перевірка віку

---
+++ lesson_05/homework_05_2.py
@@ -0,0 +1,42 @@
+people_records = [
+  ('John', 'Doe', 28, 'Engineer', 'New York'),
+  ('Alice', 'Smith', 35, 'Teacher', 'Los Angeles'),
+  ('Bob', 'Johnson', 45, 'Doctor', 'Chicago'),
+  ('Emily', 'Williams', 30, 'Artist', 'San Francisco'),
+  ('Michael', 'Brown', 22, 'Student', 'Seattle'),
+  ('Sophia', 'Davis', 40, 'Lawyer', 'Boston'),
+  ('David', 'Miller', 33, 'Software Developer', 'Austin'),
+  ('Olivia', 'Wilson', 27, 'Marketing Specialist', 'Denver'),
+  ('Daniel', 'Taylor', 38, 'Architect', 'Portland'),
+  ('Grace', 'Moore', 25, 'Graphic Designer', 'Miami'),
+  ('Samuel', 'Jones', 50, 'Business Consultant', 'Atlanta'),
+  ('Emma', 'Hall', 31, 'Chef', 'Dallas'),
+  ('William', 'Clark', 29, 'Financial Analyst', 'Houston'),
+  ('Ava', 'White', 42, 'Journalist', 'San Diego'),
+  ('Ethan', 'Anderson', 36, 'Product Manager', 'Phoenix')
+]
+
+
+# Given list of tuples (name, surname, age, profession, City location)
+# 1 - Add your new record o the beginning of the given list
+
+name = 'Tetiana'
+surname = "Nozhko"
+age = 30
+profession = "QA"
+city_location = "Odesa"
+people_records.insert(0, (name, surname, age, profession, city_location))
+print(people_records)
+
+# 2 - In modified list swap elements with indexes 1 and 5 (1<->5). Print result
+people_records[1], people_records[5] = people_records[5], people_records[1]
+print(people_records)
+
+# 3 - check that all people in modified list with records indexes 6, 10, 13
+#   have age >=30. Print condition check result
+indexes_to_check = [6, 10, 13]
+condition = [people_records[i][2] >= 30 for i in indexes_to_check]
+print("Усі мають вік >=30:", condition)
+
+
+
