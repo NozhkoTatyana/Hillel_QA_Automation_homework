4868316fbb1d94d776ef8e85139078ecaab9290c
NozhkoTatyana <tatyana.nozhko@ukr.net>
2026-03-14 00:18:19 +0200

    Виконане дз 6.3

---
+++ lesson_06/homework_06_3.py
@@ -0,0 +1,9 @@
+# Є list з даними lst1 = ['1', '2', 3, True, 'False', 5, '6', 7, 8, 'Python', 9, 0, 'Lorem Ipsum'].
+# Напишіть код, який сформує новий list (наприклад lst2), який містить лише змінні типу стрінг,
+# які присутні в lst1. Данні в лісті можуть бути будь якими
+
+lst1 = ['1', '2', 3, True, 'False', 5, '6', 7, 8, 'Python', 9, 0, 'Lorem Ipsum']
+
+lst2 = [t_str for t_str in lst1 if isinstance(t_str, str)]
+print(lst2)
+
