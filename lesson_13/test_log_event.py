94ce6277701d5f29d0ba79ebf76be340dca4128d
NozhkoTatyana <tatyana.nozhko@ukr.net>
2026-04-15 13:45:52 +0300

    Виконані зміни з урахуванням рівня логування

--- lesson_13/test_log_event.py
+++ lesson_13/test_log_event.py
@@ -14,22 +14,22 @@ class TestLogEvent(unittest.TestCase, LogFileMixin):
         log_event("userX", "success")
         self.assertTrue(os.path.exists("login_system.log"))
         content = self.read_log_file()
-        self.assertIn("Login event - Username: userX, Status: success", content)
+        self.assertIn("INFO - Login event - Username: userX, Status: success", content)
 
     def test_log_file_contains_success(self):
         log_event("user_10", "success")
         content = self.read_log_file()
-        self.assertIn("Login event - Username: user_10, Status: success", content)
+        self.assertIn("INFO - Login event - Username: user_10, Status: success", content)
 
     def test_log_file_contains_failed(self):
         log_event("user_11", "failed")
         content = self.read_log_file()
-        self.assertIn("Login event - Username: user_11, Status: failed", content)
+        self.assertIn("ERROR - Login event - Username: user_11, Status: failed", content)
 
     def test_log_file_contains_expired(self):
         log_event("user_12", "expired")
         content = self.read_log_file()
-        self.assertIn("Login event - Username: user_12, Status: expired", content)
+        self.assertIn("WARNING - Login event - Username: user_12, Status: expired", content)
 
 
 
