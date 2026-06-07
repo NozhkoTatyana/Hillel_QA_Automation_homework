43641c714982b9f5f6dbe9d78cb36aa71c09826c
NozhkoTatyana <tatyana.nozhko@ukr.net>
2026-06-07 22:48:57 +0300

    виконане дз 25

---
+++ lesson_25/css_locators.py
@@ -0,0 +1,39 @@
+# HEADER
+BTN_ABOUT = "nav.header_nav > button:nth-of-type(1)"  #1
+BTN_HOME  = "a.header-link[href='/']"  #2
+BTN_GUEST_lOGIN_IN = "button.header-link.-guest"  #3
+BTN_SIGN_IN = "button.header_signin"  #4
+
+# MAIN
+BTN_SIGN_UP = "div[class*='hero-descriptor'] > button"  #5
+VIDEO_PLAYER = "#player-controls"  #6
+BTN_PLAY_VIDEO = ".ytmCuedOverlayPlayButton"  #7
+
+# Page Garage
+BTN_MY_PROFILE = "#userNavDropdown"  #8
+DD_BTN_FUEL_EXPENSES =  "nav[class*='user'] a:nth-of-type(2)"  #9
+DD_BTN_INSTRUCTIONS = "nav[class*='user'] a:nth-of-type(3)"   #10
+DD_LOGOUT = "button.dropdown-item"  #11
+BTN_ADD_CAR = ".btn.btn-primary"  #12
+DD_BRAND_SELECT = "#addCarBrand"  #13
+OPT_BRAND_FORD = "#addCarBrand option:nth-of-type(3)" #14
+DD_MODEL_SELECT = "#addCarModel" #15
+INPUT_MILEAGE = "#addCarMileage"   #16
+BTN_CLOSE = "button.close[aria-label='Close']" #17
+
+
+# PAGE INSTRUCTIONS
+OPT_BRAND_FIAT = "div[class*='brand'] li:nth-of-type(5)"  #18
+OPT_MODEL_AUDI_TT = "#modelSelectDropdown"  #19
+BTN_SEARCH = ".instructions-search-controls_search.btn"  #20
+CARD_INSTRUCTION_SPARK_PLUGS = "div[class*='instructions_content'] li:nth-of-type(6)"  #21
+
+# Form Login
+INPUT_EMAIL = "#signinEmail"  #22
+INPUT_PASSWORD = "#signinPassword"  #23
+ERR_MSG_EMAIL_REQUIRED = "#signinEmail + div.invalid-feedback p"  #24
+BTN_LOGIN = "div.modal-footer button.btn.btn-primary"  #25
+
+
+
+
